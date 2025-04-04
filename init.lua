0988f613f96ada1015bdf2f425542bc65c9a3af3
Andy Ouyang <andyo@chislgroup.com>
2025-04-04 15:58:16 +0200

    update notpad window, update neovide title bar

--- init.lua
+++ init.lua
@@ -97,6 +97,8 @@ vim.api.nvim_create_autocmd('UIEnter', {
       vim.g.neovide_transparency = 0.8
       vim.g.neovide_floating_blur_amount_x = 2.0
       vim.g.neovide_floating_blur_amount_y = 2.0
+
+      vim.g.neovide_title_background_color = '#313244'
     end
   end,
 })
