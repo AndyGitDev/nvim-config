307d8fe48e77970b85de763105ebe2df0d68d653
Andy O <AndyO@afiahealth.com>
2025-04-07 03:38:47 -0400

    fix spelling mistake

--- lua/custom/plugins/git_worktree.lua
+++ lua/custom/plugins/git_worktree.lua
@@ -7,11 +7,12 @@ return {
     require('git-worktree').setup()
     require('telescope').load_extension 'git_worktree'
 
-    vim.keymap.set('n', '<leader>sb', "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", { desc = '[S]earch Worktree [B]ranch' })
+    vim.keymap.set('n', '<leader>sb', "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>",
+      { desc = '[S]earch Worktree [B]ranch' })
     vim.keymap.set(
       'n',
       '<leader>sB',
-      "<CMD>lua requrie('telescope').extensions.git_worktree.create_git_worktree()<CR>",
+      "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>",
       { desc = 'Create New Worktree [B]ranch' }
     )
   end,
